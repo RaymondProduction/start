49f70765a1834c623d9848a07336d4590d2ab520
raymondproduction <raymondproduction@narod.ru>
2016-09-08 11:35:33 +0300

    Modified task #05

--- task_05/js/main.js
+++ task_05/js/main.js
@@ -5,12 +5,7 @@ var numOneEl, numTwoEl, operationSignEl, resultEl;
 function exercises() {
   // Write here exercise code from http://eloquentjavascript.net/ .
   // Chapters 2, 3, and 4.
-  //--------------------------------------------------------------
-  // * Looping a triangle *
-  var symbols='';
-  for (var number = 0; number <7; number += 1)
-        console.log(symbols+='#');
-  //--------------------------------------------------------------
+
   // Flattening
   var arrays = [[1, 2, 8], [4, 5], [6]];
   // Your code here.
